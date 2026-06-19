5d0ac3b86ee98f598387456f48dbd02099c3dd77
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 23:29:51 +0900

    Time: 2 ms (43.75%), Space: 19.4 MB (39.58%) - LeetHub

--- 3581-count-odd-letters-from-number/3581-count-odd-letters-from-number.py
+++ 3581-count-odd-letters-from-number/3581-count-odd-letters-from-number.py
@@ -2,19 +2,6 @@ from collections import Counter
 
 class Solution:
     def countOddLetters(self, n: int) -> int:
-        l = [
-            "zero",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-        ]
-
-        count = Counter("".join([l[int(i)] for i in str(n)]))
-
+        numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        count = Counter("".join([numbers[int(i)] for i in str(n)]))
         return sum([v % 2 for v in count.values()])
