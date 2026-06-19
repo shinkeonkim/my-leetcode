3e8137c2a1ff9f89f95ed90f28ce2a17dc831959
kokoa <dev.shinkeonkim@gmail.com>
2026-06-20 06:53:16 +0900

    Time: 25 ms (100%), Space: 53.2 MB (88.89%) - LeetHub

--- 2758-next-day/2758-next-day.js
+++ 2758-next-day/2758-next-day.js
@@ -4,7 +4,7 @@
 Date.prototype.nextDay = function() {
     let ret = new Date(this);
     ret.setDate(ret.getDate() + 1)
-    return `${ret.getFullYear()}-${String(ret.getMonth() + 1).padStart(2, "0")}-${String(ret.getDate()).padStart(2, "0")}`
+    return ret.toISOString().substring(0, 10)
 }
 /**
  * const date = new Date("2014-06-20");
