42e95b4f3c249015d9229a07e128fb2bbe9e451f
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 15:23:06 +0900

    Time: 2346 ms (52.9%), Space: 0B (100%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -5,7 +5,8 @@ WITH CTE AS (
         o.product_id,
         product_name,
         RANK() OVER (PARTITION BY customer_id ORDER BY COUNT(*) DESC) as r
-    FROM Orders o
+    FROM Customers c
+    JOIN Orders o ON c.customer_id=o.customer_id
     JOIN products p ON o.product_id=p.product_id
     GROUP BY o.customer_id, o.product_id
 )
