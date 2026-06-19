2ac566b25f5273ebb938cb275d1ff0fb52667722
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 22:31:25 +0900

    Time: 355 ms (46.44%), Space: 90.2 MB (54.78%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    low = len(accounts[accounts['income'] < 20000])
+    high = len(accounts[accounts['income'] > 50000])
+    avg = len(accounts) - low - high
+
+    return pd.DataFrame({
+        "category": ["Low Salary", "Average Salary", "High Salary"],
+        "accounts_count": [low, avg, high]
+    })
