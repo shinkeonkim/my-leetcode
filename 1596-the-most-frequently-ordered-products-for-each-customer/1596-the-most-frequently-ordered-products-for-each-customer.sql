e66a74809907bc838056daaf9b48f13fe17a6699
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 15:23:54 +0900

    Time: 4504 ms (5.16%), Space: 0B (100%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,15 +1,11 @@
-# Write your MySQL query statement below
 WITH CTE AS (
     SELECT
         o.customer_id,
         o.product_id,
         product_name,
         RANK() OVER (PARTITION BY customer_id ORDER BY COUNT(*) DESC) as r
-    FROM Customers c
-    JOIN Orders o ON c.customer_id=o.customer_id
+    FROM Orders o
     JOIN products p ON o.product_id=p.product_id
     GROUP BY o.customer_id, o.product_id
 )
-SELECT  customer_id, product_id, product_name
-FROM CTE
-WHERE r = 1
+SELECT  customer_id, product_id, product_name FROM CTE WHERE r = 1
