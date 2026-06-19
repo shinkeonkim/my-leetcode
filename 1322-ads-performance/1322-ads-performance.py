ed3a84a4a3066ddc9e289d8813860e0ea0be88cf
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 22:48:57 +0900

    Time: 576 ms (15.02%), Space: 68.9 MB (41.2%) - LeetHub

--- 1322-ads-performance/1322-ads-performance.py
+++ 1322-ads-performance/1322-ads-performance.py
@@ -1,16 +1,10 @@
 import pandas as pd
 
 def ads_performance(ads: pd.DataFrame) -> pd.DataFrame:
-    ads = ads.groupby('ad_id')
-
-    ads = ads.agg(
+    ads = ads.groupby('ad_id').agg(
         clicked = ('action', lambda x : (x == 'Clicked').sum()),
         viewed = ('action', lambda x : (x == 'Viewed').sum())
     ).reset_index()
     
-    ads['ctr'] = round(ads['clicked'] / (ads['clicked'] + ads['viewed']) * 100, 2)
-
-    ads['ctr'] = ads['ctr'].fillna(0)
-    ads = ads.sort_values(by=['ctr', 'ad_id'], ascending=[False, True])
-
-    return ads[['ad_id', 'ctr']]
+    ads['ctr'] = (ads['clicked'] / (ads['clicked'] + ads['viewed']) * 100).round(2).fillna(0)
+    return ads.sort_values(by=['ctr', 'ad_id'], ascending=[False, True])[['ad_id', 'ctr']]
