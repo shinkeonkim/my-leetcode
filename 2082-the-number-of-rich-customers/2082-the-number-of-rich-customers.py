dfe8f943caa9aca7cb781afa9a3e9471098038b7
kokoa <dev.shinkeonkim@gmail.com>
2026-06-19 22:22:32 +0900

    Time: 286 ms (37.76%), Space: 67.3 MB (29.37%) - LeetHub

---
+++ 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def count_rich_customers(store: pd.DataFrame) -> pd.DataFrame:
+    count = store[store['amount'] > 500]['customer_id'].nunique()
+
+    return pd.DataFrame({'rich_count': [count]})
