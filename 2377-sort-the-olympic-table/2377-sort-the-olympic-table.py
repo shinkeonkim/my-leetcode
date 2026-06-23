6d3b5deb9f1dd351bb0352bbb2d3b093d61ef2d2
kokoa <dev.shinkeonkim@gmail.com>
2026-06-24 02:29:22 +0900

    Time: 328 ms (6.25%), Space: 67.7 MB (56.25%) - LeetHub

---
+++ 2377-sort-the-olympic-table/2377-sort-the-olympic-table.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def sort_table(olympic: pd.DataFrame) -> pd.DataFrame:
+    return olympic.sort_values(
+        ['gold_medals', 'silver_medals', 'bronze_medals', 'country'],
+        ascending=[False, False, False, True],
+    )
