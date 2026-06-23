35c4abc198bacf6d876adf4c39abadec4f71e54c
kokoa <dev.shinkeonkim@gmail.com>
2026-06-24 02:09:38 +0900

    Time: 651 ms (5.55%), Space: 74 MB (77.78%) - LeetHub

--- 1623-all-valid-triplets-that-can-represent-a-country/1623-all-valid-triplets-that-can-represent-a-country.py
+++ 1623-all-valid-triplets-that-can-represent-a-country/1623-all-valid-triplets-that-can-represent-a-country.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
 def find_valid_triplets(school_a: pd.DataFrame, school_b: pd.DataFrame, school_c: pd.DataFrame) -> pd.DataFrame:
-    condition = '(student_name_a != student_name_b) & (student_name_a != student_name) & (student_name_b != student_name) & ' + \
-        '(student_id_a != student_id_b) & (student_id_a != student_id) & (student_id_b != student_id)'
-    return school_a.merge(
-        school_b,
-        how='cross',
-        suffixes=('_a', '_b')
-    ).merge(
-        school_c,
-        how='cross',
-    ).query(condition).rename(
-        columns={
-            'student_name_a': 'member_A',
-            'student_name_b': 'member_B',
-            'student_name': 'member_C'
-        }
-    )[['member_A', 'member_B', 'member_C']]
+    a = school_a[['student_id', 'student_name']].rename(columns={'student_name': 'member_A', 'student_id': 'id_A'})
+    b = school_b[['student_id', 'student_name']].rename(columns={'student_name': 'member_B', 'student_id': 'id_B'})
+    c = school_c[['student_id', 'student_name']].rename(columns={'student_name': 'member_C', 'student_id': 'id_C'})
+
+    merged = a.merge(b, how='cross').merge(c, how='cross')
+
+    return merged[
+        (merged['member_A'] != merged['member_B']) & 
+        (merged['member_A'] != merged['member_C']) & 
+        (merged['member_B'] != merged['member_C']) &
+        (merged['id_A'] != merged['id_B']) & 
+        (merged['id_A'] != merged['id_C']) & 
+        (merged['id_B'] != merged['id_C'])
+    ][['member_A', 'member_B', 'member_C']]
