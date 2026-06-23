e9cee3fa3603f754cc4a006208b6b2881c842205
kokoa <dev.shinkeonkim@gmail.com>
2026-06-24 01:49:15 +0900

    Time: 364 ms (36%), Space: 69.3 MB (8%) - LeetHub

--- 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.py
+++ 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.py
@@ -1,25 +1,15 @@
 import pandas as pd
 
 def calculate_influence(salesperson: pd.DataFrame, customer: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
-    # cta = pd.merge(
-    #     salesperson,
-    #     customer,
-    #     left_on='salesperson_id',
-    #     right_on='salesperson_id',
-    #     how='left'
-    # )
-
-    cta = pd.merge(
+    return salesperson.merge(
         customer,
-        sales,
-        left_on='customer_id',
-        right_on='customer_id'
-    ).groupby('salesperson_id').agg(total=('price', 'sum')).reset_index()
-
-    return pd.merge(
-        salesperson,
-        cta,
-        left_on='salesperson_id',
-        right_on='salesperson_id',
+        on='salesperson_id',
         how='left'
-    ).fillna(0)
+    ).merge(
+        sales,
+        on='customer_id',
+        how='left'
+    ).groupby(
+        ['salesperson_id', 'name']
+    )['price'].sum().reset_index(name='total')
+
