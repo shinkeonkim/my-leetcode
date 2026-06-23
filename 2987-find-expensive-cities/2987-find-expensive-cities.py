ef093a90da128d947a4f3655a30ef80f99dc528b
kokoa <dev.shinkeonkim@gmail.com>
2026-06-24 02:45:29 +0900

    Time: 304 ms (13.33%), Space: 68.1 MB (6.67%) - LeetHub

--- 2987-find-expensive-cities/2987-find-expensive-cities.py
+++ 2987-find-expensive-cities/2987-find-expensive-cities.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def find_expensive_cities(listings: pd.DataFrame) -> pd.DataFrame:
-    grouping = listings.groupby('city', as_index=False).mean()
-    return grouping[grouping['price'] > listings['price'].mean()][['city']]
+    avg = listings['price'].mean()
+    return listings.groupby('city', as_index=False)['price'].mean().query('price > @avg')[['city']]
