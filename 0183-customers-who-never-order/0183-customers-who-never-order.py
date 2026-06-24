528be25378b3d1449d57f38a2b96cdf433b2137a
kokoa <dev.shinkeonkim@gmail.com>
2026-06-24 15:58:39 +0900

    Time: 282 ms (58.39%), Space: 67 MB (90.65%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,8 +1,4 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    merged = customers.merge(orders, left_on='id', right_on='customerId', how='left')
-
-    merged = merged[merged['customerId'].isnull()].rename(columns={'name': 'Customers'})
-
-    return merged[['Customers']]
+    return customers[~customers['id'].isin(orders['customerId'])][['name']].rename(columns={'name': 'Customers'})
