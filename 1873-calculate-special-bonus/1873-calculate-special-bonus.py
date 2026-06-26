a59faa0816ece66611f8d7fa0628569bbc84a40f
kokoa <dev.shinkeonkim@gmail.com>
2026-06-26 17:38:09 +0900

    Time: 322 ms (40.39%), Space: 67.7 MB (68.32%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = np.where(
+        (employees['employee_id'] % 2 == 1) & (~employees['name'].str.startswith('M')),
+        employees['salary'],
+        0
+    )
+
+    return employees.sort_values('employee_id')[['employee_id', 'bonus']]
