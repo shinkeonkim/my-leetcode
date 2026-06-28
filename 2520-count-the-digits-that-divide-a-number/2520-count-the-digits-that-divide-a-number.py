ea01a357eac62b1e7a473e055059307ff5b7ae90
kokoa <dev.shinkeonkim@gmail.com>
2026-06-28 16:59:45 +0900

    Time: 0 ms (100%), Space: 19 MB (98.09%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -1,3 +1,3 @@
 class Solution:
     def countDigits(self, num: int) -> int:
-        return sum(num % i == 0 for i in map(int, list(str(num))))
+        return sum(num % int(i) == 0 for i in str(num))
