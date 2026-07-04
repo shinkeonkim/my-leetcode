8025fe01cd89c1351e6ebba4ac2f93ffda944410
kokoa <dev.shinkeonkim@gmail.com>
2026-07-04 21:35:18 +0900

    Time: 0 ms (100%), Space: 8.4 MB (64.23%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ret;
+
+        int i = 0, j = 0;
+
+        while(i < word1.length() && j < word2.length()) {
+            ret += word1[i++];
+            ret += word2[j++];
+        }
+
+        while(i < word1.length()) {
+            ret += word1[i++];
+        }
+
+        while(j < word2.length()) {
+            ret += word2[j++];
+        }
+
+        return ret;
+    }
+};
